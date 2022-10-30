683d36f99cd041d9e48c1c8c18b959bda23ec51a
m.asadi <m.asadi@hpds.ir>
2022-10-30 10:37:16 +0330

    First Init

---
+++ InServer/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+iptables -t mangle -A PREROUTING -s 192.168.68.0/24 -j MARK --set-xmark 0x10000/0xff0000
+iptables -t nat -A PREROUTING -s 192.168.68.0/24 -j MARK --set-xmark 0x10000/0xff0000
+iptables -t nat -A POSTROUTING -s 192.168.68.0/24 -j MASQUERADE
+openconnect  <OutServer_IP>  --no-dtls --background --user=<USERNAME_OF_OUT_OCSERV> --passwd-on-stdin --servercert <SERVER_CERT> -s 'vpn-slice 10.12.0.0/24' < passwd.auth
+while true 
+do
+	if ip a | grep -q tun0
+	then
+		ip rule add from all fwmark 0x10000/0xff0000 lookup vpn
+		ip route add default dev tun0 table vpn 
+		break
+	fi
+	sleep 5
+done
---
+++ README.md
@@ -0,0 +1,19 @@
+# openconnect-site2site
+openconnect site2site connection 
+
+## Requirment
+
+**iproute2**
+**iptables**
+**vpn-slice(pip)**
+**openconnect**
+**ocserv**
+
+## How it's Work
+
+1. install Requirments
+2. config ocserv on Out and In servers (use configs at this repo and create certs yourself)
+3. create users for ocservs 
+4. run script.sh on InServer 
+5. connect your client to InServer
+
