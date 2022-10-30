3538ba7679669ecea62c4af8e937eb6a30a69730
m.asadi <m.asadi@hpds.ir>
2022-10-30 14:22:02 +0330

    add geoip iptables

--- InServer/script.sh
+++ InServer/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-iptables -t mangle -A PREROUTING -s 192.168.68.0/24 -j MARK --set-xmark 0x10000/0xff0000
-iptables -t nat -A PREROUTING -s 192.168.68.0/24 -j MARK --set-xmark 0x10000/0xff0000
+iptables -t mangle -A PREROUTING -s 192.168.68.0/24 -m geoip ! --destination-country  IR -j MARK --set-xmark 0x10000/0xff0000
+iptables -t nat -A PREROUTING -s 192.168.68.0/24 -m geoip ! --destination-country  IR -j MARK --set-xmark 0x10000/0xff0000
 iptables -t nat -A POSTROUTING -s 192.168.68.0/24 -j MASQUERADE
 openconnect  <OutServer_IP>  --no-dtls --background --user=<USERNAME_OF_OUT_OCSERV> --passwd-on-stdin --servercert <SERVER_CERT> -s 'vpn-slice 10.12.0.0/24' < passwd.auth
 while true 
